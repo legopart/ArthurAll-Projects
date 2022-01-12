697de9c296a1292bda2b2ff1b999a4c8ff122604
Arthur W3 <w3arthur@hotmail.com>
2022-01-12 19:10:30 +0200

    Update 1.03

---
+++ CSharpPractice/Gidi/Lecture02/ShapeDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BaseLib;
+using ShapeLib;
+using static BaseLib.MathFunctions;
+
+var rect = new Rectangle();
+
+rect.Color = System.Drawing.Color.BlueViolet;
+
+var v = new Vector2d(1, 2);
+
+var v1 = 3 * v;
+
+var v2 = vec2("(1.2323, -5)");
+
+var v3 = vec2(2,3);
+
+//-------------------------------------
+
+var s = sqrt(10);
+
+
+Console.WriteLine(v2);
+
+Console.ReadKey();
