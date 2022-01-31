ebd2678c2deaad9a921acde24e73837bdc7fb555
w3arthur <78190695+w3arthur@users.noreply.github.com>
2022-01-31 05:49:52 +0200

    update order date structure

---
+++ DataStructurePractice/4_QueueAndStack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _4_QueueAndStack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stack:");
+            Stack stack = new Stack();
+
+            stack.Push(new Item() { Name = "cola", val = 1 });
+            stack.Push(new Item() { Name = "bisli", val = 2 });
+            stack.Push(new Item() { Name = "bamba", val = 3 });
+            //stack.Pop();
+
+            Console.WriteLine(stack);
+
+
+            Console.WriteLine("Queue:");
+            Queues queue = new Queues();
+
+            queue.Push(new Item() { Name = "cola", val = 1 });
+            queue.Push(new Item() { Name = "bisli", val = 2 });
+            queue.Push(new Item() { Name = "bamba", val = 3 });
+            // queue.Pop();
+
+            Console.WriteLine(queue);
+        }
+    }
+}
---
+++ DataStructurePractice/4_QueueAndStack/Stack.cs
@@ -0,0 +1,22 @@
+﻿namespace _4_QueueAndStack
+{
+    public class Stack : ListingBasic
+    {
+        public void Push(Item newItem)
+        {
+            if (Head == null)
+            {
+                // Empty queue, this is the first item in queue
+                Head = newItem;
+                Tail = newItem;
+            }
+            else
+            {
+                newItem.Prev = Tail;
+                Tail.Next = newItem;
+                Tail = newItem;
+            }
+            alreadyInQueue++;
+        }
+    }
+}
