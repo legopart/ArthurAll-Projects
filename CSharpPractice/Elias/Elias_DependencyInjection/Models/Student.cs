2d90b8ecf147cd963b1198e04ed4911bfe37c750
w3arthur <78190695+w3arthur@users.noreply.github.com>
2022-02-02 06:14:18 +0200

    elias develop

---
+++ CSharpPractice/Elias/Elias_DependencyInjection/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elias_DependencyInjection.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
