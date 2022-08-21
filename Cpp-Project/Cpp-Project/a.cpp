893f98bac857266072f85b9693cb3b8f1d4f5081
w3arthur <78190695+w3arthur@users.noreply.github.com>
2022-08-21 19:35:41 +0300

    update

---
+++ Cpp-Project/Cpp-Project/a.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "a.h"
+	int main(){
+		std::cout << "hi1";
+		return 0;
+	}
---
+++ Cpp-Project/Cpp-Project/b.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+namespace second
+{
+
+	int main1() {
+		std::cout << "hi22";
+		return 0;
+	}
+}
