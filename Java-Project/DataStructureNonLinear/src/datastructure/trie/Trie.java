6a08b615029c7b60e91381a040abf1f71ea3c504
w3arthur <w3arthur@gmail.com>
2022-09-05 05:05:58 +0300

    trie

--- Java-Project/DataStructureNonLinear/src/datastructure/trie/Trie.java
+++ Java-Project/DataStructureNonLinear/src/datastructure/trie/Trie.java
@@ -6,35 +6,41 @@ import java.util.concurrent.ConcurrentHashMap;
 import datastructure.heap.MaxHeap;
 
 public class Trie {
-//	private static final int ALPHABET_SIZE = 26; //A..Z	//array
+	private static final int ALPHABET_SIZE = 26; //A..Z	//array
 	private class Node{
 		char value;
-//		Node[] children;
+		Node[] childrenArray;
 		HashMap<Character, Node> children;
 		boolean isEndOfWord;
 		Node(char value){
 			this.value = value;
-//			children = new Node[ALPHABET_SIZE];	//array
+			childrenArray = new Node[ALPHABET_SIZE];//array
 			children = new HashMap();
 			isEndOfWord = false;
 		}
 		@Override
 		public String toString() { return "value=" + value; }
+		Node getChild(char ch) { return children.get(ch); }
+		boolean hasChild(char ch) { return children.containsKey(ch); }
+		void addChild(char ch) { children.put(ch, new Node(ch)); }
+		
+		//array
+		Node getChildArray(char ch) { var index = ch - 'a'; return childrenArray[index]; }
+		boolean hasChildArray(char ch) { return getChildArray(ch) != null; }
+		void addChildArray(char ch) { var index = ch - 'a'; childrenArray[index] = new Node(ch); }
+		
 	}
 	private Node root;
 	public Trie() { root = new Node(' '); }
-	
+
 	private boolean isNull(Node node) {
 		return node == null;
 	}
 	public void insert(String word) {
 		var current = root;
 		for(var ch : word.toLowerCase().toCharArray()) {
-//			var index = ch - 'a';	//array
-//			if (isNull(current.children[index])) current.children[index] = new Node(ch);	//array
-			if (isNull(current.children.get(ch))) current.children.put(ch, new Node(ch));
-//			current = current.children[index];	//array
-			current = current.children.get(ch);
+			if (!current.hasChild(ch)) current.addChild(ch);
+			current = current.getChild(ch);
 		}
 		current.isEndOfWord = true;
 	}
