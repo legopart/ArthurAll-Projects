1256370af2670fef381ded3c6170d39bb1e93f6d
w3arthur <w3arthur@gmail.com>
2022-09-05 04:26:23 +0300

    trie with hashtable

--- Java-Project/DataStructureNonLinear/src/datastructure/trie/Trie.java
+++ Java-Project/DataStructureNonLinear/src/datastructure/trie/Trie.java
@@ -1,16 +1,22 @@
 package datastructure.trie;
 
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
+import datastructure.heap.MaxHeap;
+
 public class Trie {
-	private static final int ALPHABET_SIZE = 26; //A..Z
+//	private static final int ALPHABET_SIZE = 26; //A..Z	//array
 	private class Node{
 		char value;
-		Node[] childre;
+//		Node[] children;
+		HashMap<Character, Node> children;
 		boolean isEndOfWord;
 		Node(char value){
 			this.value = value;
-			childre = new Node[ALPHABET_SIZE]; 
+//			children = new Node[ALPHABET_SIZE];	//array
+			children = new HashMap();
+			isEndOfWord = false;
 		}
 		@Override
 		public String toString() { return "value=" + value; }
@@ -24,9 +30,11 @@ public class Trie {
 	public void insert(String word) {
 		var current = root;
 		for(var ch : word.toLowerCase().toCharArray()) {
-			var index = ch - 'a';
-			if (isNull(current.childre[index])) current.childre[index] = new Node(ch);
-			current = current.childre[index];
+//			var index = ch - 'a';	//array
+//			if (isNull(current.children[index])) current.children[index] = new Node(ch);	//array
+			if (isNull(current.children.get(ch))) current.children.put(ch, new Node(ch));
+//			current = current.children[index];	//array
+			current = current.children.get(ch);
 		}
 		current.isEndOfWord = true;
 	}
