a13f6c490e2c30ce4d192d6a6f0ccc4377373419
w3arthur <w3arthur@gmail.com>
2022-09-09 13:58:07 +0300

    Hash Table

--- CSharp-Project/DataStructureLinear/Hashtable/Program.cs
+++ CSharp-Project/DataStructureLinear/Hashtable/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace Hashtable
+﻿using System.Data;
+
+namespace Hashtable
 {
     class Program
     {
         static void Main()
         {
-        
+            //לחזור על HashSet
+            Dictionary<int, String> map = new Dictionary<int, String>();
+            map.Add(1, "Mosh");
+            map.Add(2, "Josh");
+            map.Add(3, "Mary");
+            map.Remove(2);
+            map.Select(i => $"{i.Key}: {i.Value}").ToList().ForEach((x) => Console.Write(" | " + x + " | "));
+            Console.WriteLine();
+            Console.WriteLine(map[3]);
+            Console.WriteLine(map.ContainsKey(3)); //O(1)
+            Console.WriteLine(map.ContainsValue("Mosh")); //O(n)
+            foreach (var item in map) Console.Write(item.Key + " " + item.Value + "\n");
+            //Values /Keys
+
+
+            Console.WriteLine(firstNonRepeatedChar("A Green Apple"));
         }
+        static public char? firstNonRepeatedChar(String str) 
+        {   // A Green Apple
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            foreach (var ch in str.ToLower()) {
+                if (map.ContainsKey(ch)) map[ch]++;
+                else map[ch] = 1;
+            }
+            foreach (var item in map) {
+                if (item.Value == 1) return item.Key;
+            }
+
+            return null;
+        }
+/*        static public String reverseString(String str) 
+        {
+        }*/
     }
 }
