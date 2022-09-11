4acb95c83f878a9005eb19913ed9b7d7b2915ae9
w3arthur <w3arthur@gmail.com>
2022-09-11 22:32:21 +0300

    bubble sort

---
+++ Java-Project/DataStructureAlgorithms/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		BubbleSort bubbleSort = new BubbleSort();
+		int[] numbers = { 7, 3, 1, 4, 6, 2, 3 };
+		bubbleSort.sort(numbers);
+		System.out.println(Arrays.toString(numbers));
+	}
+
+}
