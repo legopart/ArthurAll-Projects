04d5e241a2c2151cbc57d48dbf5d046d2152eba1
w3arthur <w3arthur@gmail.com>
2022-09-12 16:52:15 +0300

    jump search

--- Java-Project/DataStructureAlgorithms/src/searching/Main.java
+++ Java-Project/DataStructureAlgorithms/src/searching/Main.java
@@ -9,22 +9,30 @@ public class Main {
 	public static void main(String[] args) {
 		int target = 7;
 		int resultIndex;
-		int[] numbers;
-		numbers = new int[]{ 7, 3, 1, 4, 6, 2, 3 };
+		int[] numbers= new int[]{ 7, 3, 1, 4, 6, 2, 3 };
+		
 		resultIndex = LinearSearch.search(numbers, target);
 		System.out.println("LinearSearch.search \t\t" + resultIndex);
 		System.out.println("");
 		
 		numbers = new int[]{ 1, 2, 3, 3, 4, 6, 7 }; // sorted !!!
+		
 		resultIndex = BinarySearch.searchRecursion(numbers, target);
 		System.out.println("BinarySearch.searchRecursion \t" + resultIndex);
 		resultIndex = BinarySearch.searchIterative(numbers, target);
 		System.out.println("BinarySearch.searchIterative \t" + resultIndex);
 		System.out.println("");
 		
-		numbers = new int[]{ 1, 2, 3, 3, 4, 6, 7 }; // sorted !!!
 		resultIndex = TernarySearch.search(numbers, target);
 		System.out.println("TernarySearch.search \t\t" + resultIndex);
+		System.out.println("");
+		
+		resultIndex = TernarySearch.search(numbers, target);
+		System.out.println("TernarySearch.search \t\t" + resultIndex);
+		System.out.println("");
+		
+		resultIndex = JumpSearch.search(numbers, target);
+		System.out.println("JumpSearch.search \t\t" + resultIndex);
 		System.out.println("");
 	}
 
