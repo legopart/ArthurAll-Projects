6e57b10ccf3c687e943c196c82fff96a3052b7fe
w3arthur <w3arthur@gmail.com>
2022-09-12 13:45:41 +0300

    linear serch

---
+++ Java-Project/DataStructureAlgorithms/src/searching/LinearSearch.java
@@ -0,0 +1,8 @@
+package searching;
+
+public class LinearSearch {
+	public static int search(int[] array, int target) {
+		for(var i = 0; i < array.length; ++i) if(array[i] == target) return i;
+		return -1;
+	}
+}
--- Java-Project/DataStructureAlgorithms/src/searching/Main.java
+++ Java-Project/DataStructureAlgorithms/src/searching/Main.java
@@ -1,10 +1,18 @@
 package searching;
 
+import java.util.Arrays;
+
+import sorting.BubbleSort;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int index;
+		int[] numbers;
+		numbers = new int[]{ 7, 3, 1, 4, 6, 2, 3 };
+		index = LinearSearch.search(numbers, 4);
+		System.out.println("LinearSearch.search \t" + index);
+		System.out.println("");
 	}
 
 }
