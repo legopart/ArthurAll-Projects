e8ba58fd65e9ae1ac6cd38103f3b19605423417a
w3arthur <w3arthur@gmail.com>
2022-09-13 07:41:43 +0300

    bubble sort

--- CSharp-Project/DataStructureAlgorithms/Sorting/Program.cs
+++ CSharp-Project/DataStructureAlgorithms/Sorting/Program.cs
@@ -8,8 +8,17 @@
             array = new int[] { 7, 3, 1, 4, 6, 2, 3 };
             BubbleSort.Sort(array);
             Console.WriteLine("BubbleSort.Sort: " + String.Join(", ", array));
+            Console.WriteLine();
 
+            array = new int[] { 7, 3, 1, 4, 6, 2, 3 };
+            SelectionSort.Sort(array);
+            Console.WriteLine("SelectionSort.Sort: " + String.Join(", ", array));
+            array = new int[] { 7, 3, 1, 4, 6, 2, 3 };
+            SelectionSort.Sort2(array);
+            Console.WriteLine("SelectionSort.Sort2: " + String.Join(", ", array));
+            Console.WriteLine();
 
+            
         }
     }
 }
---
+++ CSharp-Project/DataStructureAlgorithms/Sorting/SelectionSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class SelectionSort
+    {
+
+        public static void Sort(int[] array)
+        {
+            for (var i = 0; i < array.Length; ++i)
+            {
+                var selection = i;
+                for (var j = i; j < array.Length - 1; ++j)
+                    if (array[selection] > array[j]) selection = j;
+                swap(array, i, selection);
+            }
+        }
+
+        public static void Sort2(int[] array)
+        {
+            for (var i = 0; i < array.Length; ++i)
+            {
+                swap(array, i, FindMinIndex(array, i));
+            }
+        }
+
+        public static int FindMinIndex(int[] array, int i)
+        {
+            var selection = i;
+            for (var j = 0; j < array.Length; ++j)
+                if (array[selection] > array[j]) selection = j;
+            return selection;
+        }
+
+        private static void swap(int[] array, int a, int b)
+        {
+            var tmp = array[a];
+            array[a] = array[b];
+            array[b] = tmp;
+        }
+    }
+}
