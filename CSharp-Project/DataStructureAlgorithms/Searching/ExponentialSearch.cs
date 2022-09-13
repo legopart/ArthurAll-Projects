92911af8b601e3ddef5f1e86040fcdc56984aec7
w3arthur <w3arthur@gmail.com>
2022-09-13 13:33:29 +0300

    exponential search

---
+++ CSharp-Project/DataStructureAlgorithms/Searching/ExponentialSearch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searching
+{
+    public class ExponentialSearch
+    {
+        public static int Search(int[] array, int target)
+        {
+            int bound = 1;
+            while (bound < array.Length && array[bound] < target) bound *= 2;
+            int[] newArray = array[(bound/2)..Math.Min(bound, array.Length)];
+            return bound/2 + Array.BinarySearch(newArray, target);//BinarySearch.SearchRecursion(newArray, target);
+        }
+    }
+}
--- CSharp-Project/DataStructureAlgorithms/Searching/Program.cs
+++ CSharp-Project/DataStructureAlgorithms/Searching/Program.cs
@@ -5,21 +5,24 @@
         static void Main()
         {
             int[] array = new int[] { 7, 3, 1, 4, 6, 2, 3 };
-            int index = 4;
+            int target = 4;
             
-            Console.WriteLine("LinearSearch.Search: " + LinearSearch.Search(array, index) );
+            Console.WriteLine("LinearSearch.Search: " + LinearSearch.Search(array, target) );
             Console.WriteLine();
 
             Array.Sort(array);  //!!! after sorting the array!
 
-            Console.WriteLine("BinarySearch.SearchIterative " + BinarySearch.SearchIterative(array, index));
-            Console.WriteLine("BinarySearch.SearchRecursion " + BinarySearch.SearchRecursion(array, index));
+            Console.WriteLine("BinarySearch.SearchIterative " + BinarySearch.SearchIterative(array, target));
+            Console.WriteLine("BinarySearch.SearchRecursion " + BinarySearch.SearchRecursion(array, target));
             Console.WriteLine();
 
-            Console.WriteLine("TernarySearch.Search " + TernarySearch.Search(array, index));
+            Console.WriteLine("TernarySearch.Search " + TernarySearch.Search(array, target));
             Console.WriteLine();
 
-            Console.WriteLine("JumpSearch.Search " + JumpSearch.Search(array, index));
+            Console.WriteLine("JumpSearch.Search " + JumpSearch.Search(array, target));
+            Console.WriteLine();
+
+            Console.WriteLine("ExponentialSearch.Search " + ExponentialSearch.Search(array, target));
             Console.WriteLine();
         }
     }
