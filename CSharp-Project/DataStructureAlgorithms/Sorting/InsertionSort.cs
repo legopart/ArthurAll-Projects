7e8cef0d7656ed6a453326826df98d1b34d96ddb
w3arthur <w3arthur@gmail.com>
2022-09-13 09:01:20 +0300

    insertion sort

---
+++ CSharp-Project/DataStructureAlgorithms/Sorting/InsertionSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class InsertionSort
+    {
+        public static void Sort(int[] array)    //לחזור
+        {
+
+            for (var i = 1; i < array.Length; ++i)  //sorted half
+            {
+                var value = array[i];
+                var place = i;
+                while(place > 0 && array[place - 1] > value)
+                {
+                    array[place] = array[place - 1];
+                    place--;
+                }
+                array[place] = value;
+            }
+        }
+    }
+}
--- CSharp-Project/DataStructureAlgorithms/Sorting/Program.cs
+++ CSharp-Project/DataStructureAlgorithms/Sorting/Program.cs
@@ -18,7 +18,12 @@
             Console.WriteLine("SelectionSort.Sort2: " + String.Join(", ", array));
             Console.WriteLine();
 
-            
+
+            array = new int[] { 7, 3, 1, 4, 6, 2, 3 };
+            InsertionSort.Sort(array);
+            Console.WriteLine("InsertionSort.Sort: " + String.Join(", ", array));
+            Console.WriteLine();
+
         }
     }
 }
