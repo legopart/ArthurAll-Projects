f5e2790a5d395aff87b0a49f2b0804b0ae4ffdae
w3arthur <w3arthur@gmail.com>
2022-09-13 07:30:09 +0300

    bubble sort

---
+++ CSharp-Project/DataStructureAlgorithms/Sorting/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public class BubbleSort
+    {
+        public static void Sort(int[] array) 
+        {
+            for (int i = 0; i < array.Length; ++i)
+            {
+                bool isSorted = true;
+                for (int j = i; j < array.Length; ++j)
+                    if (array[i] > array[j]) 
+                    {
+                        swap(array, i, j);
+                        isSorted = false;
+                    }
+                if (isSorted) return;
+            }
+        }
+        private static void swap(int[] array, int a, int b) 
+        {
+            var tmp = array[a];
+            array[a] = array[b];
+            array[b] = tmp;
+        }
+    }
+}
---
+++ CSharp-Project/DataStructureAlgorithms/Sorting/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Sorting
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array;
+            array = new int[] { 7, 3, 1, 4, 6, 2, 3 };
+            BubbleSort.Sort(array);
+            Console.WriteLine("BubbleSort.Sort: " + String.Join(", ", array));
+
+
+        }
+    }
+}
