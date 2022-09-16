137a608ef2509cf5516146cee904ff4b8f70e7b1
w3arthur <w3arthur@gmail.com>
2022-09-16 13:59:00 -0700

    Clion init2

---
+++ CLionProjects/cmake-build-debug/cmake3/main2.h
@@ -0,0 +1,14 @@
+//
+// Created by pc1 on 16/09/2022.
+//
+
+#ifndef CLIONPROJECTS_MAIN2_H
+#define CLIONPROJECTS_MAIN2_H
+
+
+class main2 {
+
+};
+
+
+#endif //CLIONPROJECTS_MAIN2_H
---
+++ CLionProjects/main2.cpp
@@ -0,0 +1,11 @@
+//
+// Created by pc1 on 16/09/2022.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World222!" << std::endl;
+
+    return 0;
+}
