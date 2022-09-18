bb14b708a1cf9651873906baa5706c7e3cb8dea4
w3arthur <w3arthur@gmail.com>
2022-09-18 15:17:35 +0300

    hashtable

--- Cpp-Clion-Project/DataStructure/Hashtable/main.cpp
+++ Cpp-Clion-Project/DataStructure/Hashtable/main.cpp
@@ -1,8 +1,12 @@
 
 
 #include <iostream>
+#include <map>
+#include <set>
 #include "HashtableChaining.cpp"
-using std::cout ;
+using std::cout, std::string, std::map, std::set ;
+static char firstNonRepeatedChar(string str);
+static char firstRepeatedChar(string str);
 int main()
 {
     cout << "hashtable \n";
@@ -23,6 +27,29 @@ int main()
     hashtable.remove(564798); // Salli
     cout << hashtable.print();
     if(hashtable.isEmpty()) cout <<  "true!\n";
-    else cout << "ok\n";
+    else cout << "ok\n\n\n";
 
+    cout << firstNonRepeatedChar("A Green Apple") << "\n"; //g
+    cout << firstRepeatedChar("A Green Apple") << "\n"; //e
+
+}
+
+
+static char firstNonRepeatedChar(string str) {
+    std::map<char, int> map {};
+    /*to lower case*/std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c){ return std::tolower(c); });
+    for (auto ch : str) {
+        if (map.contains(ch)) map.find(ch)->second ++;
+        else map.insert(std::pair(ch, 1));
+    }
+    for (auto item : map)  if (item.second == 1) return (char)item.first;
+}
+
+static char firstRepeatedChar(string str) {
+    std::set<char> set {};
+    /*to lower case*/std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c){ return std::tolower(c); });
+    for (auto ch : str) {
+        if (set.contains(ch)) return ch;
+        else set.insert(ch);
+    }
 }
