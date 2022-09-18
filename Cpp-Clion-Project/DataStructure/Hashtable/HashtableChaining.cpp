3f259627637b73e56164dd36f8709727ce771e04
w3arthur <w3arthur@gmail.com>
2022-09-18 13:44:36 +0300

    hashtable

--- Cpp-Clion-Project/DataStructure/Hashtable/HashtableChaining.cpp
+++ Cpp-Clion-Project/DataStructure/Hashtable/HashtableChaining.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <string>
 #include <list>
-using std::pair, std::list, std::string ;
+using std::pair, std::list, std::string, std::begin, std::end, std::cout ;
 
 struct Pair
 {
     int key;
     string value;
-    Pair(int key, string value) : key(key), value(value) { }
+    explicit Pair(int key, string value) : key(key), value(value) { }
+    ~Pair(){ }
 };
 
 class HashtableChaining
@@ -18,11 +19,7 @@ private:
     //list<pair<int, string>>* hashtable;
     list<Pair>* hashtable;
 
-    bool isEmpty() const
-    {
-        for (int i{}; i < HASHTABLE_LENGTH; ++i) if(hashtable[i].size() != 0) return false;
-        return true;
-    };
+
     int hashFunction(int key) const { return key % HASHTABLE_LENGTH; };
     list<Pair> getList(int key) { return hashtable[key]; };
     bool beginList(int key)
@@ -33,12 +30,19 @@ private:
     }
 public:
     HashtableChaining() : hashtable(new list<Pair>[HASHTABLE_LENGTH]) { }
+    ~HashtableChaining(){  }
+    bool isEmpty() const
+    {
+        for (int i{}; i < HASHTABLE_LENGTH; ++i) if(hashtable[i].size() != 0) return false;
+        return true;
+    };
     void insert(int key, string value)
     {
         auto& cell = hashtable[hashFunction(key)];
         bool keyExists = false;
         for(auto it = std::begin(cell);it != std::end(cell); it++){
-            if(it->key == key/*first in pair*/) {
+            if(it->key == key) /*first in pair*/
+            {
                 keyExists = true;
                 it->value = value; //new value
                 break;
@@ -48,6 +52,26 @@ public:
     }
     void remove(int key)
     {
-
+        auto& cell = hashtable[hashFunction(key)];
+        bool keyExists = false;
+        for(auto it = std::begin(cell);it != std::end(cell); it++){
+            if(it->key == key)   /*first in pair*/
+            {
+                keyExists = true;
+                it = cell.erase(it); //new iterator
+                break;
+            }
+        }
+        if(!keyExists) {cout << "item not founnd"; } //?
     }
+    void print() const
+    {
+        for(int i{}; i < HASHTABLE_LENGTH; ++i){
+            if(hashtable[i].size() == 0) continue;
+            for(auto it = hashtable[i].begin(); it != hashtable[i].end(); ++it){
+                cout << "the key:" << it->key << " value: " <<  it->value <<"\n";
+            }
+        }
+    }
+
 };
--- Cpp-Clion-Project/DataStructure/Hashtable/main.cpp
+++ Cpp-Clion-Project/DataStructure/Hashtable/main.cpp
@@ -1,9 +1,28 @@
 
 
 #include <iostream>
-//#include "LinkedList.cpp"
+#include "HashtableChaining.cpp"
 using std::cout ;
 int main()
 {
     cout << "hashtable \n";
+    HashtableChaining hashtable{};
+    if(hashtable.isEmpty()) cout <<  "true!\n";
+    hashtable.insert(345, "Arthur");
+    hashtable.insert(345677, "Bob");
+    hashtable.insert(564798, "Salli");
+    hashtable.insert(445868, "Sandy");
+    hashtable.insert(346430, "Rub");
+    hashtable.insert(548569, "John");
+
+    hashtable.print();
+    cout << "\n\n\n";
+
+    hashtable.remove(345677); //Bob
+    hashtable.remove(5798);  // -
+    hashtable.remove(564798); // Salli
+    hashtable.print();
+    if(hashtable.isEmpty()) cout <<  "true!\n";
+    else cout << "ok\n";
+
 }
