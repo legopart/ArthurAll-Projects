e110e44eb9ef70040d9006440bb42cb62868bc7f
w3arthur <w3arthur@gmail.com>
2022-09-21 13:09:51 +0300

    HeapTree

--- CSharp-Project/DataStructure/Heap/Program.cs
+++ CSharp-Project/DataStructure/Heap/Program.cs
@@ -19,18 +19,17 @@ namespace Heap
 
 
 
-        public class Kth
-        {
-            public static int getKthLargest(int[] array, int kth)
-            {
-                if (kth > array.Length || kth < 1) throw new Exception();
-                var heap = new Heap();
-                foreach (var number in array) heap.Insert(number);
 
-                for (int i = 0; i < kth - 1; i++) heap.Remove();
-                return heap.Max();//heap.Remove();
-            }
+        public static int GetKthLargest(int[] array, int kth)
+        {
+            if (kth > array.Length || kth < 1) throw new Exception();
+            var heap = new Heap();
+            foreach (var number in array) heap.Insert(number);
+
+            for (int i = 0; i < kth - 1; i++) heap.Remove();
+            return heap.Max();//heap.Remove();
         }
+
 
 
         public static void ArrayHeapify(int[] array)
--- Cpp-Clion-Project/DataStructureNonLinear/HeapTree/main.cpp
+++ Cpp-Clion-Project/DataStructureNonLinear/HeapTree/main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <list>
+#include<bits/stdc++.h>
 #include "Heap.cpp"
 using  std::cout;
 int main() {
@@ -18,3 +19,25 @@ int main() {
     cout << heap.print() << "\n";
     return 0;
 }
+
+ int getKthLargest (const int* array, const int& arrayLength,const int& kth)
+{
+    if (kth > arrayLength || kth < 1) throw new std::exception();
+    Heap heap{};
+    for (int i{}; i < arrayLength; ++i) heap.insert(array[i]);
+    for (int i = 0; i < kth - 1; i++) heap.remove();
+    return heap.max();//heap.Remove();
+}
+
+void arrayHeapify (const int* array, const int& arrayCount) { for (int i = arrayCount / 2 - 1; i >= 0; i--) arrayHeapify(array, i); }
+void arrayHeapify (int* array, const int& arrayCount,  const int& i)
+{
+    auto largerIndex = i;
+    auto leftIndex = i << 1  + 1; //i*2+1
+    auto rightIndex = i << 1 + 2; //i*2+2
+    if (leftIndex < arrayCount && array[leftIndex] > array[largerIndex]) largerIndex = leftIndex;
+    if (rightIndex < arrayCount && array[rightIndex] > array[largerIndex]) largerIndex = rightIndex;
+    if (i == largerIndex) return;
+    std::swap(array[i], array[largerIndex]);
+}
+
