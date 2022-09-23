2870dd54bd61edcdbd94dd2850b7f33363498b8c
w3arthur <w3arthur@gmail.com>
2022-09-23 06:59:23 +0300

    CanSum

--- CSharp-Project/DataStructureAlgorithms/New folder/DynamicProgramming_CanSum/Program.cs
+++ CSharp-Project/DataStructureAlgorithms/New folder/DynamicProgramming_CanSum/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace CanSum
+﻿namespace Can_Sum
 {
     class Program
     {
@@ -6,5 +6,30 @@
         {
 
         }
+
+
+        // CanSum
+
+        // canSum(7, [5, 3, 4, 7]) -> true
+        //          7
+        //      2   4   3   0   for7 , after traveling
+
+        // canSum(7, [2, 4]) -> false
+
+        public static bool CanSum(int sumTarget, int[] numbers) // O(n^m) space O(m)    m=targer sum n=array length
+        {
+            if (sumTarget == 0) return true;
+            if (sumTarget < 0) return false;
+            foreach (var num in numbers)    //after all possibilities
+            {
+                var remainder = sumTarget - num;
+                if(CanSum(remainder, numbers)) return true ;
+            }
+            return false;
+        }
+
+
+
+
     }
 }
