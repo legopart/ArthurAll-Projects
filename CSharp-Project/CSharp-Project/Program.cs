ec0cc3ef4cda6247a77acc5e2597f4f41b2f71ec
w3arthur <w3arthur@gmail.com>
2022-09-24 20:56:23 +0300

    refacturing

--- CSharp-Project/CSharp-Project/Program.cs
+++ CSharp-Project/CSharp-Project/Program.cs
@@ -3,42 +3,18 @@ using System.Transactions;
 
 namespace CSharp_Project
 {
-    //oop polymorphism example
-    class Person
-    {
-        public virtual String SayHi() 
-        {
-            return "hi";
-        }
-    }
 
-    class Italiano : Person
-    {
-        public override String SayHi() 
-        {
-            String personSayHi = base.SayHi();  //hi
-            return "olla";
-        }
 
-    }
-
-    //reverse and order the file
 
     class Program
     {
+      
         static void Main()
         {
 
-            Person person1 = new Person();  //hi
-            Console.WriteLine("Person1 sayHi: " + person1.SayHi());
-
-            Person italian1 = new Italiano();   //olla
-            Console.WriteLine("Italian sayHi: " + italian1.SayHi());
-
+            Console.WriteLine("Hello World");
 
         }
-
-
 
     }
 }
