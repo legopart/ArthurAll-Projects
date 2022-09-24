3d96d1d71bc46f4dacb5c5c36849c6caa8fba910
w3arthur <w3arthur@gmail.com>
2022-09-24 15:04:59 +0300

    1

---
+++ Cpp-Project/RestApiCSharp/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestApiCs.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : ControllerBase
+    {
+
+        private readonly ILogger<DemoController> _logger;
+
+        public DemoController(ILogger<DemoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public string Get(int aaa)
+        {
+            return $"{{\"abc\":\"{aaa}\"}}";
+        }
+
+
+    }
+}
