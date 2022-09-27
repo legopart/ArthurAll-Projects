690af543d28a56d6cab31c1e05d5a53fd21f84b9
w3arthur <w3arthur@gmail.com>
2022-09-28 01:35:32 +0300

    Jump Search

--- Cpp-Clion-Project/DataStructureAlgorithms/Searching/JumpSearch.cpp
+++ Cpp-Clion-Project/DataStructureAlgorithms/Searching/JumpSearch.cpp
@@ -1,3 +1,23 @@
-//
-// Created by pc1 on 28/09/2022.
-//
+
+#include <iostream>
+#include <string>
+using std::string, std::to_string, std::swap, std::cout, std::exception;
+
+static int jumpSearch(int*& array, const int& target, int left, int right);
+static int jumpSearch(int*& array, const int& arrayLength, const int& target)
+{
+    return jumpSearch(array, target, 0, arrayLength - 1);
+}
+
+static int jumpSearch(int*& array, const int& target, int left, int right)
+{
+    if (right < left) return -1;
+    int partitionSize = (right -left)/3;
+    int mid1 = left + partitionSize;
+    int mid2 = right - partitionSize;
+    if (array[mid1] == target) return mid1;
+    else if (array[mid2] == target) return mid2;
+    else if (target < array[mid1]) return jumpSearch(array, target, left, mid1 - 1);
+    else if (target < array[mid2]) return jumpSearch(array, target, mid1 + 1, mid2 - 1);
+    else return jumpSearch(array, target, mid2 + 1, right);
+}
--- Cpp-Clion-Project/DataStructureAlgorithms/Searching/main.cpp
+++ Cpp-Clion-Project/DataStructureAlgorithms/Searching/main.cpp
@@ -4,6 +4,7 @@
 #import "LinearSearch.cpp"
 #import "BinarySearch.cpp"
 #import "TernarySearch.cpp"
+#import "JumpSearch.cpp"
 using std::string, std::cout, std::endl;
 
 static string print(int&& result) { return (result >= 0 ? to_string(result + 1 ) + " true" : to_string(result) + " false"); }
@@ -16,11 +17,15 @@ int main()
     int* arraySorted = new int[arrayLength] { 1, 2, 3, 3, 4, 6, 7};
 
     cout <<"Linear Search: " << print( linearSearch(array, arrayLength, target) ) << endl;
+
     //using sorted array !
+
     cout <<"Binary Search: " << print( binarySearch(arraySorted, arrayLength, target) ) << endl;
     cout <<"Binary Search Recursion: " << print( binarySearch_Recursion(arraySorted, arrayLength, target) ) << endl;
 
     cout <<"Ternary Search: " << print(ternarySearch(arraySorted, arrayLength, target) ) << endl;
+
+    cout <<"Jump Search: " << print(jumpSearch(arraySorted, arrayLength, target) ) << endl;
 
 
     delete[](array);
