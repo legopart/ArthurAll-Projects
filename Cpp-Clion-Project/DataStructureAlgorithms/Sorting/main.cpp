39453c28c5643690613ace880021363e8c2515c0
w3arthur <w3arthur@gmail.com>
2022-09-27 18:26:05 +0300

    Counting Sort

--- Cpp-Clion-Project/DataStructureAlgorithms/Sorting/main.cpp
+++ Cpp-Clion-Project/DataStructureAlgorithms/Sorting/main.cpp
@@ -6,6 +6,7 @@
 #import "insertionSort.cpp"
 #import "mergeSort.cpp"
 #import "quickSort.cpp"
+#import "countingSort.cpp"
 using std::string, std::cout, std::endl;
 
 static string print(int* array, int arrayLength) { string str {"["}; for(int i = 0; i < arrayLength; ++i) str += to_string(array[i]) + ", "; str += "]\n"; return str; }
@@ -35,6 +36,9 @@ int main()
     quickSort(array, arrayLength);
     cout <<"Quick Sort: " << print(array, arrayLength);
 
+    array = new int[arrayLength] { 7, 3, 1, 4, 6, 2, 3 };
+    countingSort(array, arrayLength);
+    cout <<"Counting Sort: " << print(array, arrayLength);
 
 
     return EXIT_SUCCESS;
