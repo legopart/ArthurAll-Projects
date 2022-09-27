07d362f2652c02b812e23d873e8aeebd853e63a0
w3arthur <w3arthur@gmail.com>
2022-09-27 05:13:34 +0300

    update

--- Cpp-Clion-Project/DataStructureAlgorithms/Sorting/main.cpp
+++ Cpp-Clion-Project/DataStructureAlgorithms/Sorting/main.cpp
@@ -1,3 +1,12 @@
-//
-// Created by pc1 on 27/09/2022.
-//
+
+#include <iostream>
+#include <stack>
+//#import "Stack.cpp"
+//#import "ListedStack.cpp"
+using std::string, std::endl, std::cout;
+
+int main()
+{
+    
+    return EXIT_SUCCESS;
+}
