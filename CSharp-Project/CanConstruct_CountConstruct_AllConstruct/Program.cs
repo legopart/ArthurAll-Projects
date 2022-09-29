0a8b850fca44766dcd2714807adec603f9b3a22d
w3arthur <w3arthur@gmail.com>
2022-09-29 20:13:43 +0300

    Count Construct

--- CSharp-Project/CanConstruct_CountConstruct_AllConstruct/Program.cs
+++ CSharp-Project/CanConstruct_CountConstruct_AllConstruct/Program.cs
@@ -10,6 +10,11 @@ namespace CanConstruct_CountConstruct_AllConstruct
             Console.WriteLine(CanConstruct("skateboard", new string[] { "bo", "rd", "ate", "t", "ska", "boar" })); // false
             Console.WriteLine(CanConstruct("enterapotentpot", new string[] { "a", "p", "ent", "enter", "ot", "o", "t" })); // true
 
+            Console.WriteLine("Count Construct Brut " + CountConstructBrut("purple", new string[] { "purp", "p", "ur", "le", "purpl" })); // true
+
+            Console.WriteLine("Count Construct Memo " + CountConstruct("purple", new string[] { "purp", "p", "ur", "le", "purpl" })); // true
+            Console.WriteLine("Count Construct Memo " + CountConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeef", new string[] { "e", "ee", "eee", "eeeee", "eeeeeee" })); // true
+
             Console.WriteLine("////");
 
       //      Console.WriteLine("HowSum Brut: " + String.Join(",", HowSum(7, new int[] { 2, 3 })));
@@ -20,18 +25,73 @@ namespace CanConstruct_CountConstruct_AllConstruct
 
 
 
+        public static int CountConstruct(String target, String[] wordsBank)
+        {
+            Dictionary<string, int> memo = new Dictionary<string, int>() { /*[0] = true*/ };
+            //memo[0] = true;
+            return CountConstruct(target, wordsBank, memo);
+        }
+        private static int CountConstruct(String target, String[] wordsBank, Dictionary<string, int> memo) //  O(n^m) <-O(n^hmax) space O(m^2)    m=targer sum n=array length
+        {
+            if (target is null) return 0;
+            if (memo.ContainsKey(target)) return memo[target];
+            if (target.Length == 0) return 1;
+
+            int totalCount = 0;
+            foreach (var word in wordsBank) //only prefixes
+            {
+                if (target.IndexOf(word) == 0)
+                {
+                    var suffix = target.Substring(word.Length);
+                    var numberWays = CountConstruct(suffix, wordsBank, memo);
+                    totalCount += numberWays;
+                    
+                }
+            }
+            memo[target] = totalCount;
+            return totalCount;
+        }
+
+
+
+
+
+        public static int CountConstructBrut(String target, String[] wordsBank) //  O(n^m) <-O(n^hmax) space O(m^2)    m=targer sum n=array length
+        {
+            if (target is null) return 0;
+            if (target.Length == 0) return 1;
+            int totalCount = 0;
+            foreach (var word in wordsBank) //only prefixes
+            {
+                if (target.IndexOf(word) == 0)
+                {
+                    var suffix = target.Substring(word.Length);
+                    if (CountConstructBrut(suffix, wordsBank) == 1)
+                    {
+                        totalCount += 1;
+                    }
+                }
+            }
+            return totalCount;
+        }
+
+
+
+
+
+
         // CanSum
-        public static bool CanConstruct(String target, String[] wordsBank) //  O(n^m) <-O(n^hmax) space O(m)    m=targer sum n=array length
+        public static bool CanConstruct(String target, String[] wordsBank)
         {
             Dictionary<string, bool> memo = new Dictionary<string, bool>() { /*[0] = true*/ };
             //memo[0] = true;
             return CanConstruct(target, wordsBank, memo);
         }
-        private static bool CanConstruct(String target, String[] wordsBank, Dictionary<string, bool> memo) //  O(n^m) <-O(n^hmax) space O(m)    m=targer sum n=array length
+        private static bool CanConstruct(String target, String[] wordsBank, Dictionary<string, bool> memo) //  O(n^m) <-O(n^hmax) space O(m^2)    m=targer sum n=array length
         {
-            if(memo.ContainsKey(target)) return memo[target];
-            if (target.Length == 0) return true;
             if (target is null) return false;
+            if (memo.ContainsKey(target)) return memo[target];
+            if (target.Length == 0) return true;
             foreach (var word in wordsBank) //only prefixes
             {
                 if (target.IndexOf(word) == 0)
@@ -56,10 +116,10 @@ namespace CanConstruct_CountConstruct_AllConstruct
 
         // canSum(7, [2, 4]) -> false
 
-        public static bool CanConstructBrut(String target, String[] wordsBank) //  O(n^m) <-O(n^hmax) space O(m)    m=targer sum n=array length
+        public static bool CanConstructBrut(String target, String[] wordsBank) //  O(n^m) <-O(n^hmax) space O(m^2)    m=targer sum n=array length
         {
-            if (target.Length == 0) return true;
             if (target is null) return false;
+            if (target.Length == 0) return true;
             foreach (var word in wordsBank) //only prefixes
             {
                 if (target.IndexOf(word) == 0)
