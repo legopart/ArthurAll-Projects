2aafcf22ea325bffe263055509e54137de9a7720
w3arthur <w3arthur@gmail.com>
2022-09-30 04:55:12 +0300

    Best Sum Tabulation

--- CSharp-Project/DataStructureAlgorithms/Tabulation_CanSum_HowSum_BestSum/Program.cs
+++ CSharp-Project/DataStructureAlgorithms/Tabulation_CanSum_HowSum_BestSum/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Tabulation_CanSum_HowSum_MostReplayed_BestSum
+namespace Tabulation_CanSum_HowSum_BestSum
 {
     class Program
     {
@@ -10,8 +10,9 @@ namespace Tabulation_CanSum_HowSum_MostReplayed_BestSum
             // Time O(n) search iside the array Space O(n) for the array
             Console.WriteLine("Tabulation: ");
 
-            Console.WriteLine("Can Sum Tabulation: " + CanSumTabulation(6, new int[] { 1, 2, 3 }));
-            Console.WriteLine("How Sum Tabulation: " + String.Join(", " ,HowSumTabulation(6, new int[] { 1, 2, 3 })));
+            Console.WriteLine("Can Sum Tabulation: " + CanSumTabulation(6, new int[] { 1, 2, 3, 4 }));
+            Console.WriteLine("How Sum Tabulation: " + String.Join(", ", HowSumTabulation(6, new int[] { 1, 2, 3, 4 })));
+            Console.WriteLine("Best Sum Tabulation: " + String.Join(", ", BestSumTabulation(6, new int[] { 1, 2, 3, 4 })));
 
 
         }
@@ -20,10 +21,29 @@ namespace Tabulation_CanSum_HowSum_MostReplayed_BestSum
 
 
 
+
+        public static List<int> BestSumTabulation(int targetSum, int[] numbers) // Time O(n*m * m) => O(n*m^2) {worst case, m for the internal array size} Space O(m^2)  m for additional array internal size
+        {
+            var size = targetSum + 1;
+            var table = new List<int>[size]; //Array fill with nulls
+            table[0] = new();
+            for (int i = 0; i < size; ++i)
+            {
+                if (table[i] != null)
+                    foreach (var num in numbers)
+                        if (i + num < size) 
+                            if ( table[i + num] is null || table[i + num].Count > table[i].Count ) 
+                                table[i + num] = new(table[i]) { num };
+            }
+            return table[size - 1];
+        }
+
+
+
         public static List<int> HowSumTabulation(int targetSum, int[] numbers) // Time O(n*m * m) => O(n*m^2) {worst case, m for the internal array size} Space O(m^2)  m for additional array internal size
         {
             var size = targetSum + 1;
-            var table = new List<int>[size]; //Array fill with false
+            var table = new List<int>[size]; //Array fill with nulls
             table[0] = new();
             for (int i = 0; i < size; ++i)
             {
@@ -31,12 +51,10 @@ namespace Tabulation_CanSum_HowSum_MostReplayed_BestSum
                 {
                     foreach (var num in numbers) { 
                         if (i + num < size)
-                        {
                             table[i + num] = new(table[i]) { num };
                             //table[i + num].Add(num);
-                        }  
                         //shorter way for first result/ can work without
-                        if (table[size - 1] != null) return table[size - 1];  //faster exit
+                    //    if (table[size - 1] != null) return table[size - 1];  //faster exit
                     }
                 }
             }
