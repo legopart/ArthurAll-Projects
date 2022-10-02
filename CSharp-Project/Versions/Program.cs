b02f979906857f41fddc131e556f550db17f2b4e
w3arthur <w3arthur@gmail.com>
2022-10-03 01:41:14 +0300

    Sealed

--- CSharp-Project/Versions/Program.cs
+++ CSharp-Project/Versions/Program.cs
@@ -5,31 +5,25 @@ using UsefulLib;
 using NVI;
 
 namespace UsefulLib
-{
-    public class Base // Template Pattern, only base class will affected
-    {
+{                       //  Template Method Pattern,
+    public class Base   //  Non-Virtual Interface pattern 
+    {   //, only base class will affected
         public void DoWork() 
         {
             Console.WriteLine("Base.DoWork()");
       /*!*/ CoreDoWork();   //called the Virtal method!
             PreDoWork();
         }
-
         private void PreDoWork() { Console.WriteLine("Base.PreDoWork()"); }
-
         protected virtual void CoreDoWork() { Console.WriteLine("Base.CoreDoWork()"); }
     }
-
 }
-
-
 
 namespace NVI   //client implementation version:
 {   //no changes on client side !
     public class Derive : Base
     {
         protected override void CoreDoWork() { Console.WriteLine("Derive.CoreDoWork()"); }
-
     }
 }
 
@@ -42,10 +36,6 @@ namespace Versions
             Console.WriteLine("Versions:");
             Base instance = new Derive();
             instance.DoWork();
-
-
         }
-
     }
-
 }
---
+++ CSharp-Project/Versions_Sealed/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+
+using UsefulLib;
+using NVI;
+
+namespace UsefulLib
+{                  
+    public class Base
+    {
+        public virtual void SomeMethod1()
+        {
+            Console.WriteLine("Base.1 SomeMethod1()");
+        }
+        public virtual void SomeMethod2()
+        {
+            Console.WriteLine("Base.2 SomeMethod2()");
+        }
+    }
+}
+
+namespace NVI   //client implementation version:
+{   //no changes on client side !
+    public class Derive : Base
+    {
+        public new void SomeMethod1() { Console.WriteLine("3 New Derive.SomeMethod1()"); }
+        public sealed override void SomeMethod2() { Console.WriteLine("4 Sealed Override Derive.SomeMethod2()"); }
+
+    }
+}
+
+namespace Versions
+{
+    public class Versions
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Sealed:");
+            Base instance = new Derive();
+            instance.SomeMethod1();
+            instance.SomeMethod2();
+        }
+    }
+}
