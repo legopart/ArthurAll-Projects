cbc9f18f4f7a0d85607267a2f729f7bc3ed7b39d
w3arthur <w3arthur@gmail.com>
2022-11-19 16:56:13 +0200

    python

---
+++ Python-Project/17_classes_container.py
@@ -0,0 +1,24 @@
+
+class Product:
+    def __init__(self, price) -> None:
+        self.set_price(price)
+        pass
+
+    @property
+    def price(self):  # get_price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):  # set_price(self, value):
+        if value < 0:
+            raise ValueError("Price cannot be negative")
+        self.__price = value
+
+    # price = property(get_price, set_price)
+
+
+product = Product(10)
+print(product.price)
+
+product.price = -5  # ValueError
+# product = Product(-5)
---
+++ Python-Project/18_classes_inhiritance.py
@@ -0,0 +1,23 @@
+
+class Animal:
+    def __init__(self) -> None:
+        self.age = 1
+
+    def eat(self):
+        print("eat")
+
+
+class Mammal(Animal):
+    def walk(self):
+        print("walk")
+
+
+class Fish(Animal):
+    def swim(self):
+        print("swim")
+
+
+m = Mammal()
+m.eat()
+
+print("age", m.age)
