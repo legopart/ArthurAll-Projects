749838c8198b4abd97616c8f31e1308aa77249c6
w3arthur <w3arthur@gmail.com>
2022-11-22 04:22:39 +0200

    python

--- Python-Project/config.py
+++ Python-Project/config.py
@@ -2,3 +2,7 @@
 class Yelp:
     api_key: str = "VASA-zNJQW2nvtqKH9XobUdBTdCdumfjr1et8OuIc2zEbmzF5y8_-HgT-GJ-_jmhkNwE7TGS_hdihs-iuzt1jWOlj9WqIaReFbaR-b7CBLqe951_ytlhbrTCbxt8Y3Yx"
 
+class Twilio:
+    account_sid: str = ""
+    auth_token: str = ""
+    phone: str = ""
