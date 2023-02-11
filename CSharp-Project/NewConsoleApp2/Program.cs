6471e23783e0f566c105f40010d90350c1146909
Arthur Zarankin <78190695+w3arthur@users.noreply.github.com>
2023-02-11 19:10:59 +0200

    update

---
+++ CSharp-Project/NewConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace NewConsoleApp2
+{
+
+    class Some<T>
+    { 
+        public static int a = 1;
+    }
+
+    public class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            
+
+
+            Some<int>.a = 5;
+            Some<double>.a = 90;
+
+
+            Console.WriteLine("a is: (" + Some<int>.a + ") "); //5
+
+        }
+    }
+}
